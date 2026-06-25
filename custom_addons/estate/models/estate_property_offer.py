4fbfe42070d6bae50ecad4bc0ba6a29840c4203f
Tersom <ibnua0291@gmail.com>
2026-06-25 13:19:43 +0700

    Chapter 11: Add The Sprinkles

--- custom_addons/estate/models/estate_property_offer.py
+++ custom_addons/estate/models/estate_property_offer.py
@@ -13,6 +13,7 @@ class EstatePropertyOffer(models.Model):
     property_id = fields.Many2one('estate.property', string='Property', required=True)
     validity = fields.Integer(string='Validity (days)', default=7)
     date_deadline = fields.Date(compute='_compute_date_deadline', inverse='_inverse_date_deadline')
+    property_type_id = fields.Many2one('estate.property.type', related='property_id.estate_property_type_id' ,store=True)
 
     @api.depends('create_date', 'validity')
     def _compute_date_deadline(self):
--- custom_addons/estate/models/estate_property_type.py
+++ custom_addons/estate/models/estate_property_type.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields,api
 
 class EstatePropertyType(models.Model):
     _name = 'estate.property.type'
@@ -8,8 +8,15 @@ class EstatePropertyType(models.Model):
     name = fields.Char(required=True)
     property_ids = fields.One2many('estate.property', 'type_id',)
     sequence = fields.Integer('Sequence', default=1)
+    offer_ids = fields.One2many('estate.property.offer','property_type_id')
+    offer_count = fields.Integer(compute="_compute_offer_count")
     
     _sql_constraints = [
         ('check_unique_name', 'UNIQUE(name)',
         'Nama harus unik')
     ]
+
+    @api.depends("offer_ids")
+    def _compute_offer_count(self):
+        for record in self:
+            record.offer_count = len(record.offer_ids)
